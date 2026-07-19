bb82c26801a330476585c36d9f7ec6a2a384e642
Aswin-cs <aswinvijayan.c.s@gmail.com>
2026-07-19 20:09:16 +0530

    test: define MONGODB_URI environment variable in jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -4,4 +4,4 @@ import { TextEncoder, TextDecoder } from 'util';
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-
+process.env.MONGODB_URI = 'mongodb://localhost:27017/test_db';
